66f1839bec97983055e780ca46ee332732ba3fca
Ryan Hartadi <Ryanhartadi06@gmail.com>
2021-01-29 08:34:09 +0700

    NestedRooutes

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,22 @@
 import React from "react";
-
+import { LoginBg, RegisterBg } from "../../assets";
+import { Button, Gap, Input, Link } from "../../components";
 const Login = () => {
   return (
-    <div>
-      <p>Login Page</p>
+    <div className="main-page">
+      <div className="left">
+        <img src={RegisterBg} className="bg-image" />
+      </div>
+      <div className="right">
+        <p className="title">Login Page</p>
+        <Input name="Email" placeholder="Email" />
+        <Gap height={16} />
+        <Input name="Password" placeholder="Password" />
+        <Gap height={30} />
+        <Button title="Login" />
+        <Gap height={100} />
+        <Link title="Belum punya akun ? Silahkan daftar" />
+      </div>
     </div>
   );
 };
