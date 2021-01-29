27e174045cd4599fcad71540682b35864fc7e802
Ryan Hartadi <Ryanhartadi06@gmail.com>
2021-01-29 09:34:16 +0700

    Eps 9

---
+++ src/components/molecules/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./footer.scss";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>2020 by Ryan Hartadi</p>
+    </div>
+  );
+};
+
+export default Footer;
